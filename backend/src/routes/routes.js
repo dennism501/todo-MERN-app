1b652c3485de73de2d3fcf3edc6cee815569613e
Dennis Mubamba <dennism501@outlook.com>
2021-08-04 22:55:22 +0200

    :building_construction: Refactored the routing

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,31 +1,15 @@
 const express = require("express");
-const todo = require("../services/todo/todo");
+const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
-// TODO: Rename the route paths to something meaningful.
-router.get("/", async (req, res) => {
-  const { page, pageSize } = req.query;
-  const result = await todo.getAllTodos(page, pageSize);
+// todoController: Rename the route paths to something meaningful.
+router.get("/", todoController.getAllTodos);
 
-  res.status(200);
-  res.send(result);
-});
+router.post("/", todoController.createTodo);
 
-router.post("/", async (req, res) => {
-  const { text, dueDate } = req.body;
-  if (typeof text !== "string" && typeof dueDate !== "string") {
-    res.status(400);
-    res.json({ message: "invalid 'text' expected string" });
-    return;
-  }
-  const result = await todo.createTodo(text, dueDate);
-  res.status(201);
-  res.send(result);
-});
+router.put("/:id", todoController.updateTodo);
 
-router.put("/:id", todo.updateTodo);
-
-router.delete("/:id", todo.deleteTodo);
+router.delete("/:id", todoController.deleteTodo);
 
 module.exports = router;

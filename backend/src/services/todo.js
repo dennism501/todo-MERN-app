17f336784429fe998b2e5b3bbc75e427f3ac7539
Dennis Mubamba <dennism501@outlook.com>
2021-08-04 22:56:39 +0200

    :building_construction: Seperated the Http paramentors from the service layer

---
+++ backend/src/services/todo.js
@@ -0,0 +1,84 @@
+const { v4: generateId } = require("uuid");
+const database = require("../database/database");
+
+/**
+ *
+ * @param {Number} page
+ * @param {Number} pageSize
+ * @param {String} todayDate
+ * @returns an array of all Todos
+ */
+const getAllTodos = (page, pageSize, todayDate) => {
+  if (page === "" && pageSize === "") {
+    page = 0;
+    pageSize = 20;
+  }
+  const todos = database.client.db("todos").collection("todos");
+  const response = todos
+    .find({
+      $or: [
+        {
+          dueDate: todayDate,
+        },
+        {
+          dueDate: { $exists: !todayDate },
+        },
+      ],
+    })
+    .sort({ dueDate: -1 })
+    .limit(parseInt(pageSize, 10))
+    .skip(parseInt(pageSize, 10) * parseInt(page, 10))
+    .toArray();
+
+  return response;
+};
+
+/**
+ * @param {string} todoText The contents of the the todo to be created
+ * @param {string} dueDate The due of the day the todo
+ * @returns The object of the created todo || The error that is caught during the DB insert
+ */
+const createTodo = (todoText, dueDate) => {
+  const todo = { id: generateId(), todoText, dueDate, completed: false };
+  try {
+    database.client.db("todos").collection("todos").insertOne(todo);
+  } catch (err) {
+    return err;
+  }
+  return todo;
+};
+
+/**
+ * @param {*} id Id of the todo to be updated
+ * @param {*} completed The status of the todo
+ * @returns If an error occurs during the DB update, an err is returned
+ */
+const updateTodo = (id, completed) => {
+  try {
+    database.client
+      .db("todos")
+      .collection("todos")
+      .updateOne({ id }, { $set: { completed } });
+  } catch (err) {
+    return err;
+  }
+};
+
+/**
+ * @param {string} id Id of the todo to be deleted
+ * @returns If an error occurs during the DB update, an err is returned
+ */
+const deleteTodo = (id) => {
+  try {
+    database.client.db("todos").collection("todos").deleteOne({ id });
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = {
+  getAllTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};

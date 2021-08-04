6f6b25443e597f978b50e420c32c88e2c27e5e2c
Dennis Mubamba <dennism501@outlook.com>
2021-08-04 23:33:31 +0200

    :bulb: Updated service comments

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,7 +3,6 @@ const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
-// todoController: Rename the route paths to something meaningful.
 router.get("/api/v1/todo/get-todos", todoController.getAllTodos);
 
 router.post("/api/v1/todo/create-todo", todoController.createTodo);

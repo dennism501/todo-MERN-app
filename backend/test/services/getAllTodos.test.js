59e8d2b68fda2aa4d7e91d0afd3a041480c56105
Dennis Mubamba <dennism501@outlook.com>
2021-08-05 18:51:44 +0200

    :test_tube: More failing tests

--- backend/test/services/getAllTodos.test.js
+++ backend/test/services/getAllTodos.test.js
@@ -1,30 +1,7 @@
 const todoService = require("../../src/services/todo");
-const { MongoClient } = require("mongodb");
+const database = require("../../src/database/database");
 
 describe("✅ Tests the service layer", () => {
-  let connection;
-  let db;
-
-  beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db(global.__MONGO_DB_NAME__);
-  });
-
-  afterAll(async () => {
-    //await connection.close();
-  });
-
-  const todo = todoService(connection);
-  const querysArgs = {
-    page: 0,
-    pageSize: 20,
-    dueDate: "",
-  };
-  it("OK, Get all todos from empty collection", () => {
-    const todos = todo.getAllTodos(querysArgs.page, querysArgs.pageSize);
-    expect(todos).toEqual([]);
-  });
+  const todo = todoService(database);
+  it("OK, Get all todos from empty collection", async () => {});
 });
